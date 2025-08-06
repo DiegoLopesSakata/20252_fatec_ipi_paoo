eabb0c45b88bb90798861a8b15e061a9a0481155
Diego Lopes Sakata <dihsakata@gmail.com>
2025-08-06 18:03:39 -0300

    learn(ap01-js): encerra o codigo 1.4.1

---
+++ script.js
@@ -0,0 +1,31 @@
+// declaração de variáveis
+// const, let e var
+
+//comparação
+//operadores == e ===
+
+//hoist, içar
+// var idade = 18
+// console.log("oi, " + nome)
+// if(idade >= 18){
+//     var nome = 'João'
+//     console.log("Sim, " + nome + ". Você pode dirigir.")
+// }
+// console.log(`Até mais, ${nome}`) // precisa estar entre cráse ``
+
+// var linguagem = 'Javascript'
+// console.log("Aprendendo " + linguagem)
+// var linguagem = 'Java'
+// console.log("Aprendendo " + linguagem)
+
+// let a = 2
+// let nome = 'José'
+// console.log(a)
+// console.log(nome)
+// nome = 'José da Silva'
+// console.log(nome)
+
+// String nome = 'José'
+// const nome = 'José'
+// console.log(nome)
+// nome = 'José da Silva'
